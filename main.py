2a091fbb017058ffcbdf0ba8e96dc7fd4ae054d5
GMttL <gaby.mititelu@gmail.com>
2025-01-05 14:44:33 +0000

    added char count

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 def main():
 	with open("books/frankenstein.txt") as f:
-		file_contents = f.read()
+		text = f.read()
 	
-	print(file_contents)
-	
-	print(word_count(file_content))
+	#print(text)
+	#print(word_count(text))
+	print(char_count(text))
 
 def word_count(text):
 	return len(text.split())
+
+
+def char_count(text):
+	char_dict = {}
+	for word in text:
+		word = word.lower()
+		for char in word:
+			if char not in char_dict:
+				char_dict[char] = 1
+			else:
+				char_dict[char] = char_dict[char] + 1
+
+	return char_dict
 
 
 main()
