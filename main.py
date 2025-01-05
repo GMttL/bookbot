f08c1e0146c06ac76edde732a8918ba4ae16a543
GMttL <gaby.mititelu@gmail.com>
2025-01-05 15:04:49 +0000

    added report function

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 def main():
-	with open("books/frankenstein.txt") as f:
+	file_path = "books/frankenstein.txt"
+	with open(file_path) as f:
 		text = f.read()
-	
-	#print(text)
-	#print(word_count(text))
-	print(char_count(text))
+
+	report_gen(text, file_path)
 
 def word_count(text):
 	return len(text.split())
@@ -14,13 +13,29 @@ def char_count(text):
 	char_dict = {}
 	for word in text:
 		word = word.lower()
+		if not word.isalpha():
+			continue
 		for char in word:
 			if char not in char_dict:
 				char_dict[char] = 1
 			else:
-				char_dict[char] = char_dict[char] + 1
+				char_dict[char] += 1
 
 	return char_dict
 
+
+def report_gen(text, file_path):
+	words = word_count(text)
+	chars = char_count(text)
+	sorted_chars = dict(sorted(chars.items(), key=lambda item: item[1], reverse=True))
+
+	print(f"----- Begin report of {file_path} -----")
+	print(f"{words} words found in the document") 
+	print()
+	
+	for key, val in sorted_chars.items():
+		print(f"The '{key}' character was found {val} times")
+	
+	print("----- End Report -----")	
 
 main()
