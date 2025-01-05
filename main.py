b728ac39f3391fd373eca1615309e225afe8cdad
GMttL <gaby.mititelu@gmail.com>
2025-01-05 14:34:13 +0000

    word counts

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def main():
+	with open("books/frankenstein.txt") as f:
+		file_contents = f.read()
+	
+	print(file_contents)
+	
+	print(word_count(file_content))
+
+def word_count(text):
+	return len(text.split())
+
+
+main()
